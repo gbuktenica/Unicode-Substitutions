e4e422b0b404215d32f65703f43f2cddfa837e54
Glen Buktenica <glen.buktenica@kineticit.com.au>
2018-09-25 14:23:53 +0800

    Move const to the top

--- src/extension.ts
+++ src/extension.ts
@@ -9,6 +9,10 @@ export function activate(context: vscode.ExtensionContext) {
 
     console.log('Unicode Substitutions is activated');
 
+    // Regex rules to detect unicode characters.
+    const findRegExs = [/\u2013/g, /\u2014/g, /\u201C/g, /\u201D/g, /\u2018/g, /\u2019/g];
+    const replaceRegExs = ["\u002D", "\u002D", "\u0022", "\u0022", "\u0027", "\u0027"];
+
     // Trigger linting
     let activeEditor = vscode.window.activeTextEditor;
     if (activeEditor) {
@@ -48,9 +52,6 @@ export function activate(context: vscode.ExtensionContext) {
         context.subscriptions.push(diagnosticCollection);
         console.log(lintingRules.LintingRules);
 
-        // Regex rules to detect unicode characters.
-        const findRegExs = [/\u2013/g, /\u2014/g, /\u201C/g, /\u201D/g, /\u2018/g, /\u2019/g];
-        const replaceRegExs = ["\u002D", "\u002D", "\u0022", "\u0022", "\u0027", "\u0027"];
         findRegExs.forEach(regEx => {
             while (match = regEx.exec(text)) {
                 // Loop through each regex match and push diagnostics to array.
