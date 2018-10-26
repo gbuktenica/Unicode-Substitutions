359291e71c0badb3bbb72be2324ef4747c245ad5
Glen Buktenica <glen.buktenica@kineticit.com.au>
2018-10-26 10:59:29 +0800

    Code Acttions

--- src/extension.ts
+++ src/extension.ts
@@ -1,54 +1,60 @@
 import * as vscode from 'vscode';
 import * as defaultRulesJson from "./defaultRules.json"
-import LintingProvider from './actionProvider';
 let diagnosticCollection = null;
 diagnosticCollection = vscode.languages.createDiagnosticCollection("extensionDisplayName");
+const fixLineCommandName = "unicodesubsitutions.fixLine";
+let activeEditor = vscode.window.activeTextEditor;
+const documentSelector = {
+    "language": '*',
+    "scheme": '*'
+};
+// Read from configuration from workspace (Package.json, Settings.json etc) and default rule json.
+let supportedLanguages = vscode.workspace.getConfiguration().get('unicodesubsitutions.enabledLanguageIds');
+let enableDefaultRules = vscode.workspace.getConfiguration().get('unicodesubsitutions.enableDefaultRules');
+let enableFormatting = vscode.workspace.getConfiguration().get('unicodesubsitutions.enableFormatting');
+let lintingRules: Array<any> = [];
+lintingRules = vscode.workspace.getConfiguration().get('unicodesubsitutions.rules');
+if (enableDefaultRules) {
+    lintingRules = lintingRules.concat(defaultRulesJson.defaultRules);
+}
 
-// this method is called when vs code is activated
+//
+// Common functions
+//
+function stringToRegex(string) {
+    //Convert unicode string values to a regex global
+    let regex = new RegExp(string, 'g');
+    return regex
+}
+
+function unicodeToChar(text) {
+    //Convert a \u representation to a unicode string
+    // e.g. convert \u002D to -
+    return text.replace(/\\u[\dA-F]{4}/gi,
+        function (match) {
+            return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
+        });
+}
+//
+// This method is called when vs code is activated
+//
 export function activate(context: vscode.ExtensionContext) {
-    //
-    // Common section
-    //
     console.log('Unicode Substitutions is activated');
 
-    //Common definitions
-    let activeEditor = vscode.window.activeTextEditor;
-
-    // Read from configuration from workspace (Package.json, Settings.json etc) and default rule json.
-    let supportedLanguages = vscode.workspace.getConfiguration().get('unicodesubsitutions.enabledLanguageIds')
-    let enableDefaultRules = vscode.workspace.getConfiguration().get('unicodesubsitutions.enableDefaultRules')
-    let enableFormatting = vscode.workspace.getConfiguration().get('unicodesubsitutions.enableFormatting')
-    let lintingRules: Array<any> = [];
-    lintingRules = vscode.workspace.getConfiguration().get('unicodesubsitutions.rules');
-    if (enableDefaultRules){
-        lintingRules = lintingRules.concat(defaultRulesJson.defaultRules);
-    }
-    console.log(supportedLanguages)
-    console.log(enableDefaultRules)
-    console.log(enableFormatting)
-    console.log(lintingRules)
-
-    function stringToRegex(string) {
-        //Convert unicode string values to a regex global
-        let regex = new RegExp(string, 'g');
-        return regex
-    }
-
-    function unicodeToChar(text) {
-        //Convert a \u representation to a unicode string
-        // e.g. convert \u002D to -
-        return text.replace(/\\u[\dA-F]{4}/gi,
-            function (match) {
-                return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
-            });
-    }
     //
-    // Code Action section
+    // Code Action Section
     //
-    let linter = new LintingProvider();
-    linter.activate(context.subscriptions);
-    vscode.languages.registerCodeActionsProvider('*', linter);
 
+    // Register Code Action Provider
+    context.subscriptions.push(
+        vscode.languages.registerCodeActionsProvider(documentSelector, {
+            "provideCodeActions": provideCodeActions
+        })
+    );
+    // Register Code Action Command
+    context.subscriptions.push(
+        vscode.commands.registerCommand(fixLineCommandName, fixLine)
+    );
     //
     // Linting section
     //
@@ -86,6 +92,7 @@ export function activate(context: vscode.ExtensionContext) {
         const text = activeEditor.document.getText();
         context.subscriptions.push(diagnosticCollection);
         //Loop through each linting rule
+        let ruleIndex = 0
         lintingRules.forEach(rule => {
             let regEx = stringToRegex(rule.invalid)
             //Loop through each regex match of a rule.
@@ -95,16 +102,18 @@ export function activate(context: vscode.ExtensionContext) {
                 let range = new vscode.Range(startPos, endPos);
                 let message = rule.message;
                 let diagnostic = new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Warning);
+                diagnostic.code = ruleIndex;
                 diagnostic.source = "Unicode Substitutions";
                 diagnostics.push(diagnostic);
             }
+            ruleIndex ++
         });
         // Push diagnostics to VS Code
         diagnosticCollection.set(activeEditor.document.uri, diagnostics);
     }
 
     //
-    // Formatter section
+    // Document Formatter section (Alt + Shift +F)
     //
     if (enableFormatting) {
         vscode.languages.registerDocumentFormattingEditProvider(supportedLanguages, {
@@ -112,7 +121,7 @@ export function activate(context: vscode.ExtensionContext) {
                 console.log(lintingRules)
                 let arrayText = []
                 const text = activeEditor.document.getText();
-                let match, matchIndex
+                let match
                 //Loop through each linting rule
                 lintingRules.forEach(rule => {
                     let regEx = stringToRegex(rule.invalid)
@@ -130,4 +139,45 @@ export function activate(context: vscode.ExtensionContext) {
             }
         });
     }
+}
+
+//
+// Code Actions
+//
+
+// Implements CodeActionsProvider.provideCodeActions to provide information and fix rule violations
+function provideCodeActions(document, range, codeActionContext) {
+    const codeActions = [];
+    const diagnostics = codeActionContext.diagnostics || [];
+    diagnostics
+        .filter((diagnostic) => diagnostic.source === "Unicode Substitutions")
+        .forEach((diagnostic) => {
+            // Provide code action for information about the violation
+            const clickToFix = "Click to fix this violation";
+            const vscode = require("vscode");
+            // Provide code action to fix the violation
+            const fixTitle = clickToFix ;
+            const fixAction = new vscode.CodeAction(fixTitle, vscode.CodeActionKind.QuickFix);
+            fixAction.command = {
+                "title": fixTitle,
+                "command": fixLineCommandName,
+                "arguments": [
+                    diagnostic.range,
+                    diagnostic.code
+                ]
+            };
+            fixAction.diagnostics = [diagnostic];
+            codeActions.push(fixAction);
+        });
+    return codeActions;
+}
+
+// Fixes violations of a rule on a line
+function fixLine(range, ruleName) {
+    console.log(lintingRules[ruleName].invalid)
+    console.log(lintingRules[ruleName].valid)
+    let edit = new vscode.WorkspaceEdit();
+    let stringValid = unicodeToChar(lintingRules[ruleName].valid)
+    edit.replace(activeEditor.document.uri, range, stringValid);
+    return vscode.workspace.applyEdit(edit);
 }
