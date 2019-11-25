d0edff27079ac2c8174cb4f3dc131c0bde536912
Glen Buktenica <glen.buktenica@kloud.com.au>
2019-11-25 14:23:22 +0800

    Fomatting tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,9 +1,9 @@
 import * as assert from 'assert';
-import { after } from 'mocha';
 import * as vscode from 'vscode';
 //import * as extension from '../../extension';
 //https://github.com/microsoft/azure-pipelines-vscode/blob/master/src/test/extension.test.ts
 //https://github.com/yzhang-gh/vscode-markdown/blob/master/src/test/suite/testUtils.ts
+//https://github.com/lega911/vscode-python/blob/master/src/test/format/extension.onEnterFormat.test.ts
 
 const extensionId = "glenbuktenica.Unicode-Substitutions";
 
@@ -85,8 +85,91 @@ suite('Linting Tests', function () {
 });
 
 suite('Formatting Tests', function () {
-});
+    this.timeout(20000);
+    test('Given a bad Markdown document, after formatting there should be no validation errors', async () => {
+        // Arrange
+        const markdownFiles: vscode.Uri[] = await vscode.workspace.findFiles('badMarkdown.md');
+        const markdownFile: vscode.Uri = markdownFiles[0];
 
+        // Act
+        const markdownDocument: vscode.TextDocument = await vscode.workspace.openTextDocument(markdownFile);
+        await vscode.window.showTextDocument(markdownDocument);
+        await vscode.commands.executeCommand('editor.action.formatDocument');
+        await sleep(3000); // Give it time to show the validation errors, if any
+        const diagnostics: vscode.Diagnostic[] = vscode.languages.getDiagnostics(markdownFile);
+
+        // Assert
+        assert.equal(markdownDocument.languageId, 'markdown');
+        assert.equal(diagnostics.length, 0);
+    });
+    test('Given a bad PowerShell document, after formatting there should be no validation errors', async () => {
+        // Arrange
+        const powershellFiles: vscode.Uri[] = await vscode.workspace.findFiles('badPowerShell.ps1');
+        const powershellFile: vscode.Uri = powershellFiles[0];
+
+        // Act
+        const powershellDocument: vscode.TextDocument = await vscode.workspace.openTextDocument(powershellFile);
+        await vscode.window.showTextDocument(powershellDocument);
+        await vscode.commands.executeCommand('editor.action.formatDocument');
+        await sleep(3000); // Give it time to show the validation errors, if any
+        const diagnostics: vscode.Diagnostic[] = vscode.languages.getDiagnostics(powershellFile);
+
+        // Assert
+        assert.equal(powershellDocument.languageId, 'powershell');
+        assert.equal(diagnostics.length, 0);
+    });
+});
 async function sleep(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
+// suite('Autocomplete Tests', function() {
+//     this.timeout(30000);
+//     test ('When I use code actions then the correct characters are used.', async () => {
+//         // Arrange
+//         const markdownFiles: vscode.Uri[] = await vscode.workspace.findFiles('badMarkdown.md');
+//         const markdownFile: vscode.Uri = markdownFiles[0];
+//         const position: vscode.Position = [0];
+//         // Act
+//         const autoCompleteDoc: vscode.TextDocument = await vscode.workspace.openTextDocument(markdownFile);
+//         const editor: vscode.TextEditor = await vscode.window.showTextDocument(autoCompleteDoc);
+//         await sleep(3000); // Give it time to show the validation errors, if any
+
+//         setCursorPosition(editor, 5, 36);
+//         await vscode.commands.executeCommand('executeHoverProvider', markdownFile);
+//         const actualCompletionList = (await vscode.commands.executeCommand(
+//             'vscode.executeCompletionItemProvider',
+//             markdownFile,
+//             position
+//         )) as vscode.CompletionList;
+
+//         // Assert
+//         const diagnostics: vscode.Diagnostic[] = vscode.languages.getDiagnostics(markdownFile);
+
+//         assert.equal(autoCompleteDoc.languageId, 'markdown');
+//         assert.equal(diagnostics.length, 0);
+//     });
+// });
+
+// Set the cursor position for the editor.
+// This is 1 indexed, not 0.
+// To figure out the coordinates open the target document and look in the bottom right.
+// You don't have to make it 0 based, this function handles that for you.
+// async function setCursorPosition(editor: vscode.TextEditor, line: number, column: number) {
+//     editor.selection = new vscode.Selection(line - 1, column - 1, line - 1, column - 1);
+//     await sleep(500);
+// }
+
+
+
+// // Trigger intellisense in the UI.
+// // Make sure you already set the desired cursor position.
+// async function triggerIntellisense() {
+//     await vscode.commands.executeCommand('editor.action.triggerSuggest');
+//     await sleep(500);
+// }
+
+// // After you have called triggerIntellisense you can use this to accept the suggestion from intellisense.
+// async function acceptSuggestion() {
+//     await vscode.commands.executeCommand('acceptSelectedSuggestion');
+//     await sleep(500);
+// }
