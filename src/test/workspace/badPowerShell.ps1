f06f2846fa87e6b678c7392fdb2ace73a92fb1bd
Glen Buktenica <glen.buktenica@kloud.com.au>
2019-11-27 11:07:01 +0800

    Non breaking space rule

--- src/test/workspace/badPowerShell.ps1
+++ src/test/workspace/badPowerShell.ps1
@@ -2,4 +2,4 @@
 # Code for Unicode Substitutions to format
 Write—Output “Contains an Em dash, Start Double Quotes and End Double Quotes.”
 Write–Output ‘Contains an En dash, Start Quotes and End Quotes.’
-Write―Output ‘Contains a Horizontal Bar, Start Quotes and End Quotes.’
+Write―Output ‘Contains a Horizontal Bar, Start Quotes and End Quotes, and a non breaking space.’
